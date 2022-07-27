71322df7309262f1d740b06ed64d4b74e5b976d1
anjasganteng <102732642+anjasganteng@users.noreply.github.com>
2022-07-27 20:51:06 +0700

    Update config.js
    Zjs

--- config.js
+++ config.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['62895369318181'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['62895369318181'] //another owner number
-global.premium = ['62895369318181'] //premium number
-global.pengguna = 'AhmadChenDev' //username
-global.botnma = 'LulzGhost-Team' //bot name
+global.owner = ['6285876531805'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['6285876531805'] //another owner number
+global.premium = ['6285876531805'] //premium number
+global.pengguna = 'LoanBo-DEV' //username
+global.botnma = 'Anzas-Team' //bot name
 global.ownernma = '👻Anonim' //owner name
-global.packname = 'Koleksibot' //sticker package name
-global.author = 'By Koleksibot' //sticker author name
+global.packname = 'Sayang Anjas' //sticker package name
+global.author = 'By Anzas' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
